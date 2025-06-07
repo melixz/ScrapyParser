0a49e06e097e89d5d56d0fbe0299fdb3bb2e4d34
vlad <v.meliksetjan@idaproject.com>
2025-06-08 01:13:52 +0300

    Обновлены настройки проекта.

--- Alkoteka/settings.py
+++ Alkoteka/settings.py
@@ -1,93 +1,117 @@
-# Scrapy settings for Alkoteka project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     https://docs.scrapy.org/en/latest/topics/settings.html
-#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-
 BOT_NAME = "Alkoteka"
 
 SPIDER_MODULES = ["Alkoteka.spiders"]
 NEWSPIDER_MODULE = "Alkoteka.spiders"
 
-ADDONS = {}
+# User-Agent
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+
+# Robotstxt
+ROBOTSTXT_OBEY = False
+
+# Конфигурация запросов
+CONCURRENT_REQUESTS = 2
+DOWNLOAD_DELAY = 3
+RANDOMIZE_DOWNLOAD_DELAY = 0.5  # Рандомизация задержки
+CONCURRENT_REQUESTS_PER_DOMAIN = 16
+
+# Cookies
+COOKIES_ENABLED = True
+
+# Telnet
+TELNETCONSOLE_ENABLED = False
+
+# Заголовки запроса
+DEFAULT_REQUEST_HEADERS = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+    "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Cache-Control": "no-cache",
+    "Sec-Ch-Ua": '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": '"Windows"',
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+}
 
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-# USER_AGENT = "Alkoteka (+http://www.yourdomain.com)"
+# Middleware для паука
+SPIDER_MIDDLEWARES = {
+    "Alkoteka.middlewares.AlkotekaSpiderMiddleware": 543,
+}
 
-# Obey robots.txt rules
-ROBOTSTXT_OBEY = True
 
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
-# CONCURRENT_REQUESTS = 32
+# Middleware для загрузчика
+DOWNLOADER_MIDDLEWARES = {
+    "Alkoteka.middlewares.CookieMiddleware": 100,
+    "Alkoteka.middlewares.ProxyMiddleware": 200,
+    "Alkoteka.middlewares.UserAgentMiddleware": 300,
+    "Alkoteka.middlewares.AlkotekaDownloaderMiddleware": 543,
+    "Alkoteka.middlewares.JavaScriptMiddleware": 560,
+}
 
-# Configure a delay for requests for the same website (default: 0)
-# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
-# DOWNLOAD_DELAY = 3
-# The download delay setting will honor only one of:
-# CONCURRENT_REQUESTS_PER_DOMAIN = 16
-# CONCURRENT_REQUESTS_PER_IP = 16
 
-# Disable cookies (enabled by default)
-# COOKIES_ENABLED = False
+# Расширения
+EXTENSIONS = {
+    "scrapy.extensions.telnet.TelnetConsole": None,
+}
 
-# Disable Telnet Console (enabled by default)
-# TELNETCONSOLE_ENABLED = False
 
-# Override the default request headers:
-# DEFAULT_REQUEST_HEADERS = {
-#    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-#    "Accept-Language": "en",
-# }
+# Pipeline
+ITEM_PIPELINES = {
+    "Alkoteka.pipelines.ValidationPipeline": 100,
+    "Alkoteka.pipelines.DuplicatesPipeline": 200,
+    "Alkoteka.pipelines.AlkotekaPipeline": 300,
+    "Alkoteka.pipelines.JsonWriterPipeline": 400,
+    "Alkoteka.pipelines.StatsPipeline": 500,
+}
 
-# Enable or disable spider middlewares
-# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-# SPIDER_MIDDLEWARES = {
-#    "Alkoteka.middlewares.AlkotekaSpiderMiddleware": 543,
-# }
+# Автоматическая регулировка скорости
+AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_START_DELAY = 1
+AUTOTHROTTLE_MAX_DELAY = 10
+AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 
-# Enable or disable downloader middlewares
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-# DOWNLOADER_MIDDLEWARES = {
-#    "Alkoteka.middlewares.AlkotekaDownloaderMiddleware": 543,
-# }
+# Кэш
+HTTPCACHE_ENABLED = False
 
-# Enable or disable extensions
-# See https://docs.scrapy.org/en/latest/topics/extensions.html
-# EXTENSIONS = {
-#    "scrapy.extensions.telnet.TelnetConsole": None,
-# }
-
-# Configure item pipelines
-# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-#    "Alkoteka.pipelines.AlkotekaPipeline": 300,
-# }
-
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-# AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-# AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-# AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
-# AUTOTHROTTLE_DEBUG = False
-
-# Enable and configure HTTP caching (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-# HTTPCACHE_ENABLED = True
-# HTTPCACHE_EXPIRATION_SECS = 0
-# HTTPCACHE_DIR = "httpcache"
-# HTTPCACHE_IGNORE_HTTP_CODES = []
-# HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
-
-# Set settings whose default value is deprecated to a future-proof value
+# Кодировка
 FEED_EXPORT_ENCODING = "utf-8"
+
+# Уровень логирования
+LOG_LEVEL = "INFO"
+
+# Прокси
+PROXY_LIST = []
+
+# Cookies
+ALKOTEKA_COOKIES = {
+    "age_verified": "true",
+    "city_selected": "krasnodar",
+    "cookie_accepted": "true",
+    "adult_confirmed": "1",
+    "region": "krasnodar",
+    "cookies_accepted": "1",
+}
+
+ALKOTEKA_REGION = "krasnodar"  # Краснодар
+
+# User-Agent
+USER_AGENT_LIST = [
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0",
+]
+
+# Retry
+RETRY_ENABLED = True
+RETRY_TIMES = 3
+RETRY_HTTP_CODES = [500, 502, 503, 504, 408, 429]
+
+# Timeout
+DOWNLOAD_TIMEOUT = 30
+JAVASCRIPT_TIMEOUT = 10
